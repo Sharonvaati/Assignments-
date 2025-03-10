002e7d057ddf5e52604455d79cd235a3e1005df7
Sharonvaati <sharonvaati3@gmail.com>
2025-03-11 00:38:40 +0300

    Create For loops.C

---
+++ For loops.C
@@ -0,0 +1,17 @@
+/*
+For loops
+name: Sharon Vaati Musyoki 
+Reg no: D33-2408-2023
+Date:10/3/2025
+*/
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (int i = 1; i <= 100; i++) {
+        printf("Number is: %d and cube of the %d is: %d\n", i, i, i * i * i);
+    }
+
+    return 0;
+}
